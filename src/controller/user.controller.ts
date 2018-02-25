e144efe58c9751c550bc9c275ff07dc49e489809
pavel.kalinichenko.cr <pavel.kalinichenko.cr@gmail.com>
2018-02-25 21:19:01 +0200

    First work get /user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Get, Param, Res, HttpStatus} from "@nestjs/common";
+import {Controller, Get, Param, Res, HttpStatus, Inject} from "@nestjs/common";
 import {User} from "../entity/user";
+import {InjectRepository} from '@nestjs/typeorm';
+import {UserService} from "../service/user.service";
 
 @Controller('user')
 export class UserController {
+
+    private readonly userService: UserService;
+
+    constructor(@InjectRepository(User)
+                    userService: UserService) {
+        this.userService = userService;
+    }
+
     @Get()
-    findAll(@Res() res): User[] {
-        return res.status(HttpStatus.OK).json([]);
+    async findAll(@Res() res): Promise<User[]> {
+        return res.status(HttpStatus.OK).json({this.userService.findAll()});
     }
 
     @Get(':id')
-    findOne(@Res() res, @Param() params): User {
+    async findOne(@Res() res, @Param() params): User {
         console.log(params.id);
         return res.status(HttpStatus.OK).json({});
     }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,10 +1,17 @@
-import { Component } from "@nestjs/common";
-import { User } from "../entity/user";
+import {Component} from "@nestjs/common";
+import {getRepository} from "typeorm";
+import {User} from "../entity/user";
 
 @Component()
 export class UserService {
 
-    findAll(): User[] {
-        return [];
+    async findAll(id: number): Promise<User[]> {
+        const userRepository = getRepository<User>(User); // you can also get it via getConnection().getRepository() or getManager().getRepository()
+        return await userRepository.find();
+    }
+
+    async findById(id: number): Promise<User> {
+        const userRepository = getRepository<User>(User); // you can also get it via getConnection().getRepository() or getManager().getRepository()
+        return await userRepository.findOneById(id);
     }
 }
