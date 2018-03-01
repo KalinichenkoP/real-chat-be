591c41e53c7cf0af20023a8892f11999f730a5e9
pavelkalinichenkocr <pavel-cleveroad@octopuslabs.com>
2018-03-01 16:40:44 +0200

    Clear code

--- src/models/message/message.module.ts
+++ src/models/message/message.module.ts
@@ -3,8 +3,6 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {MessageService} from "./message.service";
 import {MessageController} from "./message.controller";
 import {Message} from "./message";
-import {MessageProvider} from "./message.provider";
-import {MongoEntityManager} from "typeorm";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Message])],
