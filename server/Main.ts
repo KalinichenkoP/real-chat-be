2a974c05c72d6ce0de605cc4331e5fb0a40dcdd2
pavel.kalinichenko.cr <pavel.kalinichenko.cr@gmail.com>
2018-07-12 00:08:39 +0300

    Add chat page, implemented chat through router and material

--- server/Main.ts
+++ server/Main.ts
@@ -5,7 +5,24 @@ async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule, { cors: true });
     app.setGlobalPrefix("/api/v1");
     const port = 3000;
-    await app.listen(port);
+    const server = await app.listen(port);
+
+    const io = require('socket.io')(server);
+    const redisAdapter = require('socket.io-redis');
+    io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
+
+    var your_namespace_socket = io.of('/your-namespace');
+    your_namespace_socket.on('connection', function(socket){
+        console.log('connected');
+        socket.on('join', function(room){
+            socket.join(room);
+
+            //log other socket.io-id's in the room
+            your_namespace_socket.adapter.clients([room], (err, clients) => {
+                console.log(clients);
+            });
+        });
+    });
 }
 
 bootstrap();
