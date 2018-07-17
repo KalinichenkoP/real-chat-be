42aec180c6f6ed55a06270cd9d1f000ea38cc4b0
pavel <pavel.kalinichenko.cr@gmail.com>
2018-07-18 01:18:29 +0300

    try add redis

--- server/models/events/EventsGateway.ts
+++ server/models/events/EventsGateway.ts
@@ -1,14 +1,14 @@
 import { SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
 import {from, Observable} from 'rxjs';
 import { map } from 'rxjs/operators';
+import * as RedisAdapter from "socket.io-redis";
 
-@WebSocketGateway(3001)
+@WebSocketGateway(3001, {adapter: RedisAdapter({host:'localhost', port: 6379})})
 export class EventsGateway {
     @WebSocketServer() server;
 
     @SubscribeMessage('events')
     onEvent(client, data): Observable<WsResponse<number>> {
-        console.log(client);
         console.log(data);
         const event = 'events';
         const response = [1, 2, 3];
